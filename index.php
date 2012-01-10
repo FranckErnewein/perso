cd59624552f6decc0375f951e5d624a4cb15d5f3
unknown <franck@franck-PC.(none)>
2012-01-10 11:11:43 +0100

    remove old index

--- index.php
+++ index.php
@@ -1,115 +1,47 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Franck Ernewein</title>
-		<script type="text/javascript" src="js/lib/jquery-1.7.1.min.js"></script>
-		<script type="text/javascript" src="js/lib/underscore.js"></script>
-		<script type="text/javascript" src="js/lib/backbone.js"></script>
-		<script type="text/javascript" src="js/script.js"></script>
-        <style>
-            @import url(reset.css);
-            @import url(style.css);
-        </style>
-    </head>
-    <body>
-        <div id="global">
-			<div id="content" >
-                
-				<div class="trame"></div>
-<!--
-				<iframe src="http://www.novalistaitbout.com/" width="100%" height="100%" frameborder="0"></iframe>
-                -->
-                <div class="articles">
-                    <article>
-                        <h3>Generali</h3>
-                        <a href="#">http://generali.fr</a><br />
-                        <span>HTML</span><br />
-                        <span>Javascript</span><br />
-                        <span>CSS</span>
-                    </article>
-                    <article style="opacity:0.1">
-                        <h3>Novalis</h3>
-                        <a href="#">http://generali.fr</a><br />
-                        <span>HTML</span><br />
-                        <span>Javascript</span><br />
-                        <span>CSS</span>
-                    </article>
-                    <article style="opacity:0.1">
-                        <h3>Generali</h3>
-                        <a href="#">http://generali.fr</a><br />
-                        <span>HTML</span><br />
-                        <span>Javascript</span><br />
-                        <span>CSS</span>
-                    </article>
-                </div>
-
-			</div>
-			<div style="display:none;">
-				<div class="trame"></div>
-				<img src="img/media/generali/1.jpg" alt="" class="bg" />
-                <!--
-                <div class="cloud">
-                    site institutionnel Générali<br />
-                    portail réaliser dans lorem ipsum dolor.
-                </div>
-                -->
-                <div class="info">
-                    
-                    <!--
-                    
-                    
-                    
-                    <ul>
-                        <li>HTML</li>
-                        <li>Javascript</li>
-                        <li>CSS</li>
-                    </ul>
-                    <br /><br /><br /><br />
-                    -->
-                    <div class="ct-info">
-                        <a href="#">http://generali.fr</a>
-                        <span>HTML</span>
-                        <span>Javascript</span>
-                        <span>CSS</span>
-                    </div>
-                </div>
-                <a class="next" href="#next"><img src="img/next.png" alt="&gt;"/></a>
-                <a class="prev" href="#prev"><img src="img/prev.png" alt="&lt;"/></a>
-			</div>
-            <div id="nav">
-				<hgroup>
-					<h1><a href="#">Franck Ernewein</a></h1>
-					<h2>Front end developper</h2>
-				</hgroup>
-                <ul>
-					<li>
-						<a href="#skills">+ Sketch</a>
-						<ul>
-							<li><a href="sketch/mucus">Mucus</a></li>
-							<li><a href="sketch/invasion">Invasion</a></li>
-							<li><a href="sketch/trom">Trom</a></li>
-							<li><a href="sketch/particule">Particule</a></li>
-							<li><a href="sketch/hypnosis">Hypnosis</a></li>
-							<li><a href="morphingweb">Morphing Web</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="#works">+ Works</a>
-						<ul>
-							<li><a href="#">Generali</a></li>
-							<li><a href="#">Novalis</a></li>
-							<li><a href="#">Generali</a></li>
-							<li><a href="#">Novalis</a></li>
-							<li><a href="#">Generali</a></li>
-							<li><a href="#">Novalis</a></li>
-							<li><a href="#">Generali</a></li>
-							<li><a href="#">Novalis</a></li>
-						</ul>
-					</li>
-                    <li><a href="#contact">+ About</a></li>
-                </ul>
-            </div>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html lang="fr-fr">
+<head>
+	<meta charset="utf-8" />
+	<title>Franck Ernewein</title>
+	<meta name="author" content="Franck Ernewein" />
+	<style type="text/css">
+		@import url(reset.css);
+		@import url(style.css);
+	</style>
+	<script src="js/lib/jquery-1.7.1.min.js"></script>
+    <script src="js/lib/jquery.randomize.js"></script>
+	<script src="js/lib/underscore.js"></script>
+	<script src="js/lib/backbone.js"></script>
+    <script src="js/lib/processing-1.3.6.min.js"></script>
+	<script src="js/model.js"></script>
+	<script src="js/collection.js"></script>
+	<script src="js/router.js"></script>
+	<script src="js/view.js"></script>
+	<script src="js/app.js"></script>
+    <script>
+        app.collection.sketchs.add( <?php include 'data/sketch.json'; ?>);
+        app.collection.works.add( <?php include 'data/work.json'; ?>);
+    </script>
+</head>
+<body>
+<div id="global">
+	<div class="fx-mask"></div>
+	<div id="head">
+		<hgroup>
+			<h1><a href="#">Franck Ernewein</a></h1>
+			<h2>Front end developper</h2>
+		</hgroup>
+	</div>
+	<div id="content">
+		<div id="nav">
+			<ul>
+				<li><a href="#sketch">Sketch</a></li>
+				<li><a href="#work">Works</a></li>
+				<li><a href="#about">About</a></li>
+			</ul>
+		</div>
+		<div id="page"></div>
+	</div>
+</div>
+</body>
 </html>
